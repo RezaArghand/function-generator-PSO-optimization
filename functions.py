2b49e6c171761da7aac2a734b8a00d6f265689bc
mrrigidbody <arghand.eng@gmail.com>
2022-02-14 18:18:36 +0330

    first commitment

---
+++ functions.py
@@ -0,0 +1,28 @@
+
+
+
+# Function to check parentheses
+def isBalanced(myStr):
+    open_list = ["[", "{", "("]
+    close_list = ["]", "}", ")"]
+    stack = []
+    for i in myStr:
+        if i in open_list:
+            stack.append(i)
+        elif i in close_list:
+            pos = close_list.index(i)
+            if ((len(stack) > 0) and
+                    (open_list[pos] == stack[len(stack) - 1])):
+                stack.pop()
+            else:
+                return "Unbalanced"
+    if len(stack) == 0:
+        return True
+    else:
+        return False
+
+
+
+
+
+
